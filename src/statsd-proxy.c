9dbcb1d75dd83695e9929a8c12cada0b63bc4536
hit9 <hit9@icloud.com>
2015-09-08 14:38:46 +0800

    Main entry

---
+++ src/statsd-proxy.c
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
+ *
+ * Proxy for etsy/statsd.
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+#include "log.h"
+#include "proxy.h"
+
+#include "config.h"
+
+int
+main(int argc, const char *argv[])
+{
+    log_open("statsd-proxy", NULL);
+
+    pthread_t threads[num_threads];
+    struct ctx *ctxs[num_threads];
+
+    int i;
+
+    for (i = 0; i < num_threads; i++) {
+        ctxs[i] = ctx_new(nodes, num_nodes, port);
+        pthread_create(&threads[i], NULL, &server_start, ctxs[i]);
+    }
+
+    for (i = 0; i < num_threads; i++) {
+         pthread_join(threads[i], NULL);
+    }
+
+    log_close();
+    return 0;
+}

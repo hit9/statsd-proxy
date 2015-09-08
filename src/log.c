da08662548928ac6524b6bc6d256a23b17e8816a
hit9 <hit9@icloud.com>
2015-09-08 14:35:17 +0800

    Enable logging threadsafe (mutexlock)

---
+++ src/log.c
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "log.h"
+#include "utils.h"
+
+static struct logger logger;
+
+/* Open global logger, if `filename` is NULL, use stderr. */
+int
+log_open(char *name, char *filename)
+{
+    assert(name != NULL);
+
+    struct logger *l = &logger;
+
+    l->name = name;
+    l->level = LOG_INFO;
+    l->filename = NULL;
+
+    if (filename == NULL) {
+        l->fd = STDERR_FILENO;
+    } else {
+        l->filename = filename;
+        l->fd = open(filename, LOG_FILE_PERM, LOG_FILE_MODE);
+
+        if (l->fd < 0) {
+            return LOG_EOPEN;
+        }
+    }
+
+    pthread_mutex_init(&(l->lock), NULL);
+    return LOG_OK;
+}
+
+/* Close global logger. */
+void
+log_close(void)
+{
+    struct logger *l = &logger;
+
+    if (l->fd < 0 || l->fd == STDERR_FILENO)
+        return;
+    close(l->fd);
+}
+
+/* Reopen logging file. */
+int
+log_reopen(void)
+{
+    struct logger *l = &logger;
+
+    if (l->fd < 0 || l->fd == STDERR_FILENO)
+        return LOG_OK;
+
+    close(l->fd);
+
+    assert(l->filename != NULL);
+
+    pthread_mutex_lock(&(l->lock));
+    l->fd = open(l->filename, LOG_FILE_PERM, LOG_FILE_MODE);
+    pthread_mutex_unlock(&(l->lock));
+
+    if (l->fd < 0) {
+        return LOG_EOPEN;
+    }
+
+    return LOG_OK;
+}
+
+/* Set logger's level. */
+void
+log_setlevel(int level)
+{
+    struct logger *l = &logger;
+    l->level = MAX(MIN(LOG_CRITICAL, level), LOG_DEBUG);
+}
+
+/* Format logging message to file/stderr. */
+int
+log_log(int level, char * levelname, const char *fmt, ...)
+{
+    struct logger *l = &logger;
+
+    assert(levelname != NULL);
+    assert(l->name != NULL);
+    assert(l->fd == STDERR_FILENO || l->fd > 0);
+
+    if (level < l->level)
+        return LOG_OK;
+
+    pthread_mutex_lock(&(l->lock));
+
+    int len = 0, size = LOG_LINE_LEN_MAX;
+
+    char buf[size + 1];
+
+    // readable time with ms
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    len += strftime(buf + len, size - len, "%Y-%m-%d %H:%M:%S.", localtime(&tv.tv_sec));
+    len += _scnprintf(buf + len, size - len, "%03ld", tv.tv_usec/1000);
+    // level
+    len += _scnprintf(buf + len, size - len, " %s", levelname);
+    // name and pid and tid
+    len += _scnprintf(buf + len, size - len, " %s[%ld/%ld] ", l->name, getpid(), pthread_self());
+
+    va_list args;
+    va_start(args, fmt);
+    len += _vscnprintf(buf + len, size - len, fmt, args);
+    va_end(args);
+
+    buf[len++] = '\n';
+
+    if (write(l->fd, buf, len) < 0) {
+        return LOG_EWRITE;
+    }
+
+    pthread_mutex_unlock(&(l->lock));
+    return LOG_OK;
+}
---
+++ src/log.h
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
+ *
+ * Logging implementation.
+ */
+
+#ifndef _CW_LOG_H
+#define _CW_LOG_H 1
+
+#include <fcntl.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#define LOG_LINE_LEN_MAX    256
+#define LOG_FILE_MODE       0644
+#define LOG_FILE_PERM       O_WRONLY | O_APPEND | O_CREAT
+
+
+#define LOG_DEBUG_S         "debug"
+#define LOG_INFO_S          "info"
+#define LOG_WARN_S          "warn"
+#define LOG_ERROR_S         "error"
+#define LOG_CRITICAL_S      "critical"
+
+enum {
+    LOG_DEBUG = 10,
+    LOG_INFO = 20,
+    LOG_WARN = 30,
+    LOG_ERROR = 40,
+    LOG_CRITICAL = 50,
+};
+
+enum {
+    LOG_OK = 0,     /* operation is ok */
+    LOG_EOPEN = 1,  /* failed to open file */
+    LOG_EWRITE = 2, /* failed to write to file */
+};
+
+struct logger {
+    char *name;             /* logger name */
+    char *filename;         /* filename to log */
+    int level;              /* logging level */
+    int fd;                 /* fd to write */
+    pthread_mutex_t lock;   /* lock on logging */
+};
+
+#define log_debug(...)      log_log(LOG_DEBUG, LOG_DEBUG_S, __VA_ARGS__)
+#define log_info(...)       log_log(LOG_INFO, LOG_INFO_S, __VA_ARGS__)
+#define log_warn(...)       log_log(LOG_WARN, LOG_WARN_S, __VA_ARGS__)
+#define log_error(...)      log_log(LOG_ERROR, LOG_ERROR_S, __VA_ARGS__)
+#define log_critical(...)   log_log(LOG_CRITICAL, LOG_CRITICAL_S, __VA_ARGS__)
+
+int log_open(char *name, char *filename);
+void log_close(void);
+int log_reopen(void);
+void log_setlevel(int level);
+int log_log(int level, char *levelname, const char *fmt, ...);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif

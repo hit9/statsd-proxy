6faabbf73dfaae062fe5ef10dbd000a46976c728
hit9 <hit9@icloud.com>
2015-09-10 20:54:40 +0800

    Support for custom cfg

---
+++ src/config.c
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
+ *
+ * Proxy config.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "buf.h"
+#include "cfg.h"
+#include "config.h"
+#include "ketama.h"
+#include "log.h"
+
+#define CONFIG_READ_UNIT    1024
+
+struct config *
+config_new(void)
+{
+    struct config *c = malloc(sizeof(struct config));
+
+    if (c == NULL)
+        return NULL;
+
+    c->port = 8125;
+    c->num_threads = 4;
+
+    int i;
+
+    for (i = 0; i < KETAMA_NUM_NODES_MAX; i++) {
+        if ((c->nodes[i].key = malloc(
+                        KETAMA_NODE_KEY_LEN_MAX * sizeof(char))) == NULL) {
+            for (i = 0; i < KETAMA_NUM_NODES_MAX; i++)
+                if (c->nodes[i].key != NULL)
+                    free(c->nodes[i].key);
+            return NULL;
+        }
+    }
+
+    return c;
+}
+
+void
+config_free(struct config *c)
+{
+    if (c != NULL) {
+        int i;
+        for (i = 0; i < KETAMA_NUM_NODES_MAX; i++)
+            if (c->nodes[i].key != NULL)
+                free(c->nodes[i].key);
+        free(c);
+    }
+}
+
+
+int
+config_init(struct config *c, const char *filename)
+{
+    assert(c != NULL);
+
+    struct buf *buf = buf_new(NULL);
+
+    /* Read config file */
+    int nread;
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        log_error("cannot open file %s", filename);
+        return CONFIG_EFOPEN;
+    }
+
+    while (1) {
+        if (buf_grow(buf, buf->len + CONFIG_READ_UNIT) != BUF_OK) {
+            log_error("no memory error");
+            return CONFIG_ENOMEM;
+        }
+
+        if ((nread = fread(buf->data + buf->len, sizeof(char),
+                    buf->cap - buf->len, fp)) <= 0)
+            break;
+
+        buf->len += nread;
+    }
+
+    /* Parse and get values */
+    struct cfg cfg;
+    cfg.data = buf->data;
+    cfg.len = buf->len;
+    cfg.lineno = 1;
+
+    int cfg_err;
+    c->num_nodes = 0;
+
+    while ((cfg_err = cfg_get(&cfg)) == CFG_OK) {
+        size_t s_len = cfg.val_len + 1;
+        char s[s_len];
+        memset(s, s_len, 0);
+
+        if (strncmp("port", cfg.key, cfg.key_len) == 0) {
+            memcpy(s, cfg.val, cfg.val_len);
+            c->port = (unsigned short)strtol(s, NULL, 10);
+            if (c->port == 0) {
+                log_error("invalid port at line %d", cfg.lineno);
+                return CONFIG_EVALUE;
+            }
+        }
+
+        if (strncmp("num_threads", cfg.key, cfg.key_len) == 0) {
+            memcpy(s, cfg.val, cfg.val_len);
+            c->num_threads = (size_t)strtol(s, NULL, 10);
+            if (c->num_threads == 0) {
+                log_error("invalid num_threads at line %d", cfg.lineno);
+                return CONFIG_EVALUE;
+            }
+        }
+
+        if (strncmp("node", cfg.key, cfg.key_len) == 0) {
+            memcpy(s, cfg.val, cfg.val_len);
+            if (strlen(s) >= KETAMA_NODE_KEY_LEN_MAX) {
+                log_error("node address too large at line %d", cfg.lineno);
+                return CONFIG_EVALUE;
+            }
+            char host[cfg.val_len];
+            unsigned short port;
+            unsigned short weight;
+
+            if (sscanf(s, "%[^:]:%hu:%hu", host, &port, &weight) != 3) {
+                log_error("invalid node at line %d", cfg.lineno);
+                return CONFIG_EVALUE;
+            }
+
+            struct ketama_node node = c->nodes[c->num_nodes++];
+            sprintf(node.key, "%s:%hu", host, port);
+            node.weight = weight;
+            log_info("load statsd node %s:%hu:%hu done.", host, port, weight);
+        }
+    }
+
+    buf_free(buf);
+
+    if (cfg_err == CFG_EBADFMT) {
+        log_error("invalid syntax in %s, at line %d", filename,
+                cfg.lineno);
+        return CONFIG_EBADFMT;
+    }
+    return CONFIG_OK;
+}
--- src/statsd-proxy.c
+++ src/statsd-proxy.c
@@ -8,7 +8,9 @@
 #error "statsd-proxy requires linux3.9+"
 #endif
 
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 #include "log.h"
@@ -16,26 +18,64 @@
 
 #include "config.h"
 
+#define STATSD_PROXY_VERSION    "0.0.3"
+
+void usage(void);
+void start(struct config *config);
+
 int
 main(int argc, const char *argv[])
 {
+    if (argc != 2)
+        usage();
+
     log_open("statsd-proxy", NULL);
 
-    pthread_t threads[num_threads];
-    struct ctx *ctxs[num_threads];
+    struct config *config = config_new();
+
+    if (config == NULL)
+        exit(1);
+
+    if (config_init(config, argv[1]) != CONFIG_OK)
+        exit(1);
+
+    start(config);
+
+    config_free(config);
+    log_close();
+    return 0;
+}
+
+void
+usage(void)
+{
+    fprintf(stderr, "usage: ./statsd-proxy /path/to/config.cfg\n");
+    fprintf(stderr, "version: %s\n", STATSD_PROXY_VERSION);
+    exit(1);
+}
+
+void
+start(struct config *config)
+{
+    assert(config != NULL);
+    assert(config->num_threads > 0);
+    assert(config->port > 0);
+    assert(config->nodes != NULL);
+    assert(config->num_nodes > 0);
+
+    pthread_t threads[config->num_threads];
+    struct ctx *ctxs[config->num_threads];
 
     int i;
 
-    for (i = 0; i < num_threads; i++) {
-        ctxs[i] = ctx_new(nodes, num_nodes, port);
+    for (i = 0; i < config->num_threads; i++) {
+        if ((ctxs[i] = ctx_new(config->nodes, config->num_nodes, config->port)) == NULL)
+            exit(1);
         pthread_create(&threads[i], NULL, &thread_start, ctxs[i]);
     }
 
-    for (i = 0; i < num_threads; i++) {
+    for (i = 0; i < config->num_threads; i++) {
          pthread_join(threads[i], NULL);
          ctx_free(ctxs[i]);
     }
-
-    log_close();
-    return 0;
 }
